27958499c8b326e476a2b2313661afc799949b5a
Chia <ChiaYi.Lai@georgebrown.ca>
2019-01-30 14:08:31 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let balance = 0;
-let name = `George Brown`;
+let name = `John Doe`;
 
 
 // Greet the user by name
@@ -25,9 +25,11 @@ var completeTransaction = function() {
   let _amount = parseInt(document.getElementById('amount').value);
 
   // 1. Update the balance using the transaction value
+  // https://stackoverflow.com/questions/10160850/difference-between-and-in-javascript
   balance += _amount;
 
   // 2. Add a record to the history showing the amount deposited
+  //https://stackoverflow.com/questions/4234533/how-do-i-concatenate-a-string-with-a-variable
   document.getElementById('history').innerHTML += '<li>You deposited $' + _amount + '.</li>';
 
   // Update the balance in the document
@@ -35,21 +37,15 @@ var completeTransaction = function() {
 
   // 3. IF the balance is less than 0, make the balance red, else make it black
         // A) change the style.color of the balance
+        // https://stackoverflow.com/questions/17925577/change-text-color-with-javascript
+        if (balance < 0){
+          document.getElementById('balance').style.color = "red";
+        }else{
+          document.getElementById('balance').style.color = "black";
+        }
         // B) create a css class and .add() a class to the balance
-};
 
-if (balance < 0){
-  document.getElementById('balance').style.color = "red";
-}else{
-  document.getElementById('balance').style.color = "black";
-}
-
-var style = document.createElement('style');
-style.type = 'text/css';
-style.innerHTML = '.newCssClass {font-family: \'Lobster\', cursive;}';
-document.getElementsByTagName('head')[0].appendChild(style);
-document.getElementById('balance').className = 'newCssClass';
-};
+      };
 
 document.getElementById('process').addEventListener('click', completeTransaction)
 
